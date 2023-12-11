6e272d4b64aec43a3867cebc42ad5772c27c851a
bibartanjha <32161986+bibartanjha@users.noreply.github.com>
2023-12-10 21:34:37 -0600

    Add files via upload

--- wordle-java/src/Wordle.java
+++ wordle-java/src/Wordle.java
@@ -1,1 +1,113 @@
+// A simple program to demonstrate
+// Tic-Tac-Toe Game.
+import java.util.*;
 
+public class Wordle {
+
+    static String userWordToGuess;
+    static Map<Character, Set<Integer>> lettersWithPlacements;
+    static boolean keepPlayingGame = true;
+
+    static Scanner in = new Scanner(System.in);
+
+    static final int totalTurns = 7;
+    static final int lettersInWord = 5;
+
+
+    public static void main(String[] args)
+    {
+        while (keepPlayingGame) {
+            startNewRound();
+        }
+        in.close();
+    }
+
+    public static void startNewRound() {
+        getWordAndCreateLetterMap();
+        System.out.println("\n\nYou have " + totalTurns + " turns to guess a " + lettersInWord + "-letter word. Enter q to quit the current round at any time?");
+        playGame();
+    }
+
+    public static void playGame() {
+        String userInput;
+
+        int numCorrectlyPlacedLetters;
+        int [] resultsOfUserInput = new int [lettersInWord]; // 0 = not in word, 1 = in word but wrong placements, 2 = correct placement
+
+        boolean stillPlaying = true;
+        int numTurnsLeft = totalTurns;
+
+        while (stillPlaying) {
+            userInput = in.nextLine();
+            if (userInput.equals("q")) {
+                System.out.println("You have entered q to quit the current round. Press a to re-start a new round, and Press b to quit the game entirely");
+                userInput = in.nextLine();
+                if (userInput.equals("b")) {
+                    keepPlayingGame = false;
+                }
+                break;
+            }
+            if (userInput.length() != lettersInWord) {
+                System.out.println("Please enter a " + lettersInWord + "-letter word");
+                continue;
+            }
+            Arrays.fill(resultsOfUserInput, 0);
+            numCorrectlyPlacedLetters = 0;
+            for (int i = 0; i < lettersInWord; i ++) {
+                char letter = userInput.charAt(i);
+                if (lettersWithPlacements.containsKey(letter)) {
+                    if (lettersWithPlacements.get(letter).contains(i)) {
+                        resultsOfUserInput[i] = 2;
+                        numCorrectlyPlacedLetters ++;
+                    } else {
+                        resultsOfUserInput[i] = 1;
+                    }
+                } else {
+                    resultsOfUserInput[i] = 0;
+                }
+            }
+
+            for (int number : resultsOfUserInput) {
+                System.out.print(number + " ");
+            }
+            System.out.println();
+
+            if (numCorrectlyPlacedLetters == lettersInWord) {
+                System.out.println("Congrats! The word was " + userWordToGuess + ". You win in " + (totalTurns - numTurnsLeft + 1) + " guesses");
+                System.out.println("Press a to re-start a new round, and Press b to quit the game entirely");
+                userInput = in.nextLine();
+                if (userInput.equals("b")) {
+                    keepPlayingGame = false;
+                }
+                stillPlaying = false;
+            }
+
+            numTurnsLeft --;
+            System.out.println("You have " + numTurnsLeft + " guesses");
+            if (numTurnsLeft == 0) {
+                System.out.println("You did not guess the word correctly. The word was " + userWordToGuess);
+                System.out.println("Press a to re-start a new round, and Press b to quit the game entirely");
+                userInput = in.nextLine();
+                if (userInput.equals("b")) {
+                    keepPlayingGame = false;
+                }
+                stillPlaying = false;
+            }
+        }
+    }
+
+    public static void getWordAndCreateLetterMap() {
+        // Get new word
+        // Get the instance of APIDataManager
+        WordsFromApi wordsFromApi = WordsFromApi.getInstance();
+        String word = wordsFromApi.getRandomWordFromList();
+        userWordToGuess = word;
+
+        //setup hash map
+        lettersWithPlacements = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            lettersWithPlacements.computeIfAbsent(letter, k -> new HashSet<>()).add(i);
+        }
+    }
+}
---
+++ wordle-java/src/WordsFromApi.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
+
+public class WordsFromApi {
+    private static WordsFromApi instance = new WordsFromApi();
+    private String[] wordsList;
+
+    private WordsFromApi() {
+        try {
+            String apiUrl = "https://api.datamuse.com/words?sp=?????";
+
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == 200) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+
+                reader.close();
+                String jsonResponse = response.toString();
+                wordsList = parseString(jsonResponse);
+            } else {
+                wordsList = new String[0];
+                System.err.println("Failed to retrieve a word. HTTP Status: " + responseCode);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String[] parseString(String allWordsString) {
+
+        String[] seperatedWords = allWordsString.split(",\"score\":\\d+},\\{\"word\":");
+        seperatedWords[0] = seperatedWords[0].replace("[{\"word\":", "");
+        seperatedWords[seperatedWords.length - 1] = seperatedWords[seperatedWords.length - 1].split(",\"score\":\\d+}]")[0];
+
+        for (int i = 0; i < seperatedWords.length; i ++) {
+            seperatedWords[i] = seperatedWords[i].replace("\"", "");
+        }
+
+        /*
+        System.out.println(seperatedWords[0]);
+        System.out.println(seperatedWords[1]);
+        System.out.println(seperatedWords[2]);
+        System.out.println(seperatedWords[3]);
+        System.out.println(seperatedWords[seperatedWords.length - 1]);
+         */
+
+        return seperatedWords;
+    }
+
+    public static WordsFromApi getInstance() {
+        return instance;
+    }
+
+    public String[] getWordsList() {
+        return wordsList;
+    }
+
+    public String getRandomWordFromList() {
+        // Create a Random object
+        Random random = new Random();
+
+        // Generate a random index within the bounds of the array
+        int randomIndex = random.nextInt(wordsList.length);
+
+        // Get the random item from the array
+        return wordsList[randomIndex];
+    }
+}
