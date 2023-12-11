d7c852c627988518cae2d92e4978d4a3d29618dd
bibartanjha <32161986+bibartanjha@users.noreply.github.com>
2023-12-11 01:03:15 -0600

    Add files via upload

--- wordle-kotlin/build.gradle.kts
+++ wordle-kotlin/build.gradle.kts
@@ -1,1 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+plugins {
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.serialization") version "1.5.21"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-client-core:2.2.4")
+    implementation("io.ktor:ktor-client-json:2.2.4")
+    implementation("io.ktor:ktor-client-gson:2.2.4")
+    implementation("io.ktor:ktor-client-okhttp:2.2.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
